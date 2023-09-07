5158aa4e3460d634e0dd8684cee12728fcea8781
sjdj0147 <sjdj014@gamil.com>
2023-09-07 13:30:12 +0900

    first commit

---
+++ app/src/main/java/com/example/mybookmark/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mybookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.mybookmark.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;    //멤버변수
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //뷰페이저가 프레그먼트 3개와 연결하고자한다.
+
+
+
+        //수정
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+}
